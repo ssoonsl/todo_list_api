3d405f6a0a6c2efa25c85aa50d35581341953633
ssoonsl <ssoonsl616+gh@gmail.com>
2017-11-28 16:56:26 +0800

    Change complete datatype on tasks

--- db/migrate/20171128040104_create_tasks.rb
+++ db/migrate/20171128040104_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :complete
+      t.boolean :complete
 
       t.timestamps
     end
